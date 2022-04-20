da419f77755e1fe43868744768070bc21be47695
Abid Ali <abid@torque360.co>
2022-04-20 12:28:33 +0500

    form validation

--- src/Components/PagesComponents/Home/Form/Form.jsx
+++ src/Components/PagesComponents/Home/Form/Form.jsx
@@ -1,41 +1,81 @@
-import React from 'react'
+import React from 'react';
 import styles from './Form.module.css';
-const Form = ({ data, handleChange }) => {
+const Form = ({ data, handleChange, handleSubmit, errors }) => {
   return (
     <form className={styles.container}>
       <h1 className={styles.title}>Personal Info</h1>
       <div className={styles.userImg}>
-        <label htmlFor="user"><img width="100%" src={data?.userImg ?? "./assets/images/user.svg"} alt="user" /></label>
-        <input onChange={handleChange} type="file" name="user" id="user" />
+        <label htmlFor='user'>
+          <img
+            width='100%'
+            src={data?.userImg ?? './assets/images/user.svg'}
+            alt='user'
+          />
+        </label>
+        <input onChange={handleChange} type='file' name='user' id='user' />
       </div>
       <div className={styles.inputContainer}>
-        <label htmlFor="name">NAME</label>
-        <input name="name" onChange={handleChange} type="text" id="name" value={data?.name ?? ""} />
+        <label htmlFor='name'>NAME</label>
+        <input
+          name='name'
+          onChange={handleChange}
+          type='text'
+          id='name'
+          className={errors?.name && styles.error}
+          value={data?.name ?? ''}
+        />
       </div>
       <div className={styles.inputContainer}>
-        <label htmlFor="email">EMAIL</label>
-        <input name="email" onChange={handleChange} type="email" id="email" value={data?.email ?? ""} />
+        <label htmlFor='email'>EMAIL</label>
+        <input
+          name='email'
+          onChange={handleChange}
+          type='email'
+          className={errors?.email && styles.error}
+          id='email'
+          value={data?.email ?? ''}
+        />
       </div>
       <div className={styles.inputContainer}>
-        <label htmlFor="dob">DATE OF BIRTH</label>
-        <input name="dob" onChange={handleChange} type="date" id="dob" value={data?.dob ?? ""} />
+        <label htmlFor='dob'>DATE OF BIRTH</label>
+        <input
+          name='dob'
+          onChange={handleChange}
+          type='date'
+          id='dob'
+          className={errors?.dob && styles.error}
+          value={data?.dob ?? ''}
+        />
       </div>
       <div>
         <h4>GENDER</h4>
         <div className={styles.genderContainer}>
           <div>
-            <label htmlFor="male">Male</label>
-            <input onChange={handleChange} type="radio" name='gender' id="male" />
+            <label htmlFor='male'>Male</label>
+            <input
+              onChange={handleChange}
+              type='radio'
+              name='gender'
+              defaultChecked
+              id='male'
+            />
           </div>
           <div>
-            <label htmlFor="female">Female</label>
-            <input onChange={handleChange} type="radio" name='gender' id="female" />
+            <label htmlFor='female'>Female</label>
+            <input
+              onChange={handleChange}
+              type='radio'
+              name='gender'
+              id='female'
+            />
           </div>
         </div>
       </div>
-      <button type='submit' className={styles.submitBtn}>Submit</button>
+      <button type='submit' onClick={handleSubmit} className={styles.submitBtn}>
+        Submit
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,33 +1,117 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Form from '../../Components/PagesComponents/Home/Form/Form';
 import Posts from '../../Components/PagesComponents/Home/Posts/Posts';
 
 const Home = () => {
-    const [isAuth, setIsAuth] = useState(true);
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        dob: "",
-        gender: 'Male',
+  const [errors, setErrors] = useState({
+    name: false,
+    email: false,
+    dob: false,
+  });
 
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    dob: '',
+    gender: 'Male',
+  });
+
+  const [isAuth, setIsAuth] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
     });
-    const handleChange = e => {
-        const { name, value } = e.target;
-        setData({
-            ...data,
-            [name]: value
-        })
+    setErrors({
+      ...errors,
+      [name]: false,
+    });
+  };
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+  const checkValidation = () => {
+    const validName = /^[A-Za-z ]+$/.test(data.name);
+    const validEmail = validateEmail(data.email);
+    const validDOB =
+      new Date().getFullYear() >= new Date(data.dob).getFullYear();
+    switch (true) {
+      case !validName:
+        setErrors({
+          ...errors,
+          name: true,
+        });
+        return false;
+      case !validEmail:
+        setErrors({
+          ...errors,
+          email: true,
+        });
+        return false;
+      case !validDOB:
+        setErrors({
+          ...errors,
+          dob: true,
+        });
+        return false;
+      default:
+        return true;
     }
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = checkValidation();
+    if (isValid) {
+      toast.success('Success Notification !', {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      setIsAuth(localStorage.setItem('isAuth', true));
+      setErrors({
+        name: false,
+        email: false,
+        dob: false,
+      });
+    }
+    return false;
+  };
 
-    return (
-        <>
-            {
-                isAuth ? <Form handleChange={handleChange} data={data} /> : <Posts />
-            }
+  const handleLogout = () => {
+    localStorage.setItem('isAuth', true);
+  };
+  const authorisation = localStorage.getItem('isAuth');
+  useEffect(() => {
+    if (authorisation) {
+      setIsAuth(localStorage.setItem('isAuth', false));
+    }
+  }, [authorisation]);
 
-        </>
-    )
-}
+  return (
+    <>
+      <ToastContainer />
+      <button onClick={handleLogout}>Log out</button>
+      {console.log(isAuth)}
+      {isAuth ? (
+        <Posts />
+      ) : (
+        <Form
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          errors={errors}
+          data={data}
+        />
+      )}
+    </>
+  );
+};
 
-export default Home
+export default Home;
